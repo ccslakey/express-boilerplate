2996ceda57f30524b075a3e1fb030fbbdd3b2f3c
ccslakey <cromwellslakey@gmail.com>
2015-12-01 12:56:16 -0800

    added bootstrap and express

--- server.js
+++ server.js
@@ -1,32 +1,31 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
-var topLevelPath = __dirname
-var viewsPath = topLevelPath + '/views/';
+var path = __dirname + '/views/';
 
-router.use(function(req, res, next) {
-    console.log("/" + req.method);
-    next();
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
 });
 
-router.get("/", function(req, res) {
-    res.sendFile(viewsPath + "index.html");
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
 });
 
-router.get("/about", function(req, res) {
-    res.sendFile(viewsPath + "about.html");
+router.get("/about",function(req,res){
+  res.sendFile(path + "about.html");
 });
 
-router.get("/contact", function(req, res) {
-    res.sendFile(viewsPath + "contact.html");
-});
+router.get("/contact",function(req,res){
+  res.sendFile(path + "contact.html");
+  });
 
-app.use("/", router);
+  app.use("/",router);
 
-app.use("*", function(req, res) {
-    res.sendFile(viewsPath + "404.html");
-});
+  app.use("*",function(req,res){
+    res.sendFile(path + "404.html");
+  });
 
-app.listen(3000, function() {
+  app.listen(3000,function(){
     console.log("Live at Port 3000");
-});
+  });
